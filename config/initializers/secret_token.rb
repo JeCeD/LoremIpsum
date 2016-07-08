dff3c1671acb54e639f8768ec1286866a69537fe
JeCeD <jeced@hotmail.com>
2016-07-08 16:41:59 -0500

    Nuevo Statuses Index Final

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoremIpsum::Application.config.secret_key_base = '1a32c96336409e24b83ff4742ed567fd14bb43e4c1d332c2af56614cd429df46643fd99d9f88ddeb6df3f5e32be9198f92cad4523fda81767390383da460451d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoremIpsum::Application.config.session_store :cookie_store, key: '_LoremIpsum_session'
